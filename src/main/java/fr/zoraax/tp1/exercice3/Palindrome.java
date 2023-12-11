9899e97695cda48cdc299fbd6d981bf33d1d5ac7
Valentin Thibault <valentin.thlt@gmail.com>
2023-12-11 15:04:49 +0100

    refacto: refacto code and add javadoc

--- src/main/java/fr/zoraax/tp1/exercice3/Palindrome.java
+++ src/main/java/fr/zoraax/tp1/exercice3/Palindrome.java
@@ -12,22 +12,38 @@ public class Palindrome {
         isPalindrome(inputValue);
     }
 
+    /**
+     * Renvoi la valeur vraie ou fausse si le nombre est un palindrome ou pas.
+     * @param value
+     * @return True ou False en fonction du résultat
+     */
     private static boolean isPalindrome(int value) {
         int palindromeValue = value;
         int somme = 0;
 
+        somme = checkPalindromeValue(palindromeValue, somme);
+
+        if (somme == value) {
+            System.out.println(value + " est un palindrome");
+            return true;
+        } else {
+            System.out.println(value + " n'est pas un palindrome");
+            return false;
+        }
+    }
+
+    /**
+     * Vérifie la valeur saisie si c'est un palindrome ou non
+     * @param palindromeValue
+     * @param somme
+     * @return La somme de la valeur saisie
+     */
+    private static int checkPalindromeValue(int palindromeValue, int somme) {
         while (palindromeValue != 0) {
            int newPalindromeValue = palindromeValue % 10;
             somme = somme * 10 + newPalindromeValue;
             palindromeValue /= 10;
         }
-
-        if (somme == value) {
-            System.out.println(value + "est un palindrome");
-            return true;
-        } else {
-            System.out.println(value + "n'est pas un palindrome");
-            return false;
-        }
+        return somme;
     }
 }
